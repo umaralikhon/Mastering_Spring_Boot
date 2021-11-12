d3b6f0edcee5852e432c0ec9deae23f0e54013c8
Umaralikhon <umaralikhon@gmail.com>
2021-11-12 16:57:23 +0500

    EnableConfigurationProperties

---
+++ src/main/java/com/example/mastering_spring_boot/AccountsClientConfig.java
@@ -0,0 +1,23 @@
+package com.example.mastering_spring_boot;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(ConnectionSettings.class)
+public class AccountsClientConfig {
+    ConnectionSettings connectionSettings;
+
+    public AccountsClientConfig(ConnectionSettings connectionSettings){
+        this.connectionSettings = connectionSettings;
+    }
+
+    @Bean
+    public void displayAccountClientConfig(){
+        System.out.println("ACCOUNT CLIENT CONFIG");
+        System.out.println("Host: " + connectionSettings.getHost());
+        System.out.println("Port: " + connectionSettings.getPort());
+        System.out.println();
+    }
+}
