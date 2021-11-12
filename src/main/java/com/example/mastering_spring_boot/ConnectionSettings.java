579352506fed935dd7b99a56719a49f0f3c6b7db
Umaralikhon <umaralikhon@gmail.com>
2021-11-12 16:29:54 +0500

    Initial commit

---
+++ src/main/java/com/example/mastering_spring_boot/ConnectionSettings.java
@@ -0,0 +1,24 @@
+package com.example.mastering_spring_boot;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "account.client")
+@Data
+public class ConnectionSettings {
+    private String host;
+    private int port;
+    private String logDir;
+    private int timeout;
+
+    @Bean
+    public void display(){
+        System.out.println("Host: " + host);
+        System.out.println("Port: " + port);
+        System.out.println("LogDir: " + logDir);
+        System.out.println("Timeout: " + timeout);
+    }
+}
---
+++ src/main/java/com/example/mastering_spring_boot/MasteringSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.mastering_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteringSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasteringSpringBootApplication.class, args);
+    }
+
+}
